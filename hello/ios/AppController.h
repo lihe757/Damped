945d90511a22b53e1b94c2fc52ce61d107f1a64d
lihex <lihe757@gmail.com>
2011-12-03 20:52:28 +0800

    Initial Commit

---
+++ hello/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  helloAppController.h
+//  hello
+//
+//  Created by 李帅 on 12/3/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
